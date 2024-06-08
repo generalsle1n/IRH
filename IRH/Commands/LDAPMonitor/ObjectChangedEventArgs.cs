eeed8fb203c64a4ddc1a6890ebc3a9291baf7a7d
Niels Schuler <niels.schuler@web.de>
2024-06-08 19:41:52 +0200

    Added Eventargs

---
+++ IRH/Commands/LDAPMonitor/ObjectChangedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.LDAPMonitor
+{
+    internal class ObjectChangedEventArgs : EventArgs
+    {
+        public SearchResultEntry Result { get; set; }
+
+        public ObjectChangedEventArgs(SearchResultEntry entry)
+        {
+            Result = entry;
+        }
+    }
+}
