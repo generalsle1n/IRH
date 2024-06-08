610b75e9b10d99db1b59766d71920f5d4bbf10b4
Niels Schuler <niels.schuler@web.de>
2024-06-08 19:20:15 +0200

    renamed file

---
+++ IRH/Main.cs
@@ -0,0 +1,18 @@
+﻿using IRH.Commands;
+using Serilog;
+using Serilog.Core;
+using System.CommandLine;
+
+Logger Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Verbose()
+    .CreateLogger();
+
+RootCommand RootCommand = new RootCommand();
+
+LDAPMonitor LM = new LDAPMonitor(Logger);
+Command LdapMonitor = LM.CreateCommand(RootCommand);
+
+RootCommand.AddCommand(LdapMonitor);
+
+await RootCommand.InvokeAsync(args);
