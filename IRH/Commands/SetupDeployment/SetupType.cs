e70e9fda7b8df71a4508f983389f4b819c8f5a22
Niels Schuler <niels.schuler@web.de>
2024-06-09 10:23:32 +0200

    Added Enum

---
+++ IRH/Commands/SetupDeployment/SetupType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.SetupDeployment
+{
+    internal enum SetupType
+    {
+        WinRM,
+        PSExec
+    }
+}
