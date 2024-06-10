122d55875daa0e9b537baf08f8449ea44770f704
Niels Schuler <niels.schuler@web.de>
2024-06-10 22:07:45 +0200

    Added Parameter to Interface

--- IRH/Commands/SetupDeployment/Types/ISetupType.cs
+++ IRH/Commands/SetupDeployment/Types/ISetupType.cs
@@ -5,7 +5,7 @@ namespace IRH.Commands.SetupDeployment.Types
     public interface ISetupType
     {
         public string SourceBinary { get; set; }
-        public string[] Parameters { get; set; }
+        public string Parameters { get; set; }
         public string DestinationPC { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
