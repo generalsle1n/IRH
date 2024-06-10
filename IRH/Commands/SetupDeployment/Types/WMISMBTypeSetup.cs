1efe6832def7d8fac6c5a824e31869c3923e4b90
Niels Schuler <niels.schuler@web.de>
2024-06-10 19:17:33 +0200

    Added WmI Setup

---
+++ IRH/Commands/SetupDeployment/Types/WMISMBTypeSetup.cs
@@ -0,0 +1,27 @@
+﻿using Serilog.Core;
+using SimpleImpersonation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.SetupDeployment.Types
+{
+    public class WMISMBTypeSetup : ISetupType
+    {
+        public string SourceBinary { get; set; }
+        public string[] Parameters { get; set; }
+        public string DestinationPC { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Logger Logger { get; set; }
+
+        public bool Install()
+        {
+            //Impersonation a = new Impersonation()
+            File.Copy(SourceBinary, @$"\\{DestinationPC}\c$\temp\lol");
+            return false;
+        }
+    }
+}
