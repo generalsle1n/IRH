7bd2ab588342e8f5638e22e916ad9ed073b52f66
Niels Schuler <niels.schuler@web.de>
2024-06-10 19:17:14 +0200

    Added Interface for Setup

---
+++ IRH/Commands/SetupDeployment/Types/ISetupType.cs
@@ -0,0 +1,22 @@
+﻿using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.SetupDeployment.Types
+{
+    public interface ISetupType
+    {
+        public string SourceBinary { get; set; }
+        public string[] Parameters { get; set; }
+        public string DestinationPC { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Logger Logger { get; set; }
+
+        public bool Install();
+    }
+}
