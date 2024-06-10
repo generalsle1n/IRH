8f42471a9bc621e5e6e44cc3ec30a6a40007a63f
Niels Schuler <niels.schuler@web.de>
2024-06-10 22:03:41 +0200

    Required Usings

--- IRH/Commands/SetupDeployment/SetupDeployment.cs
+++ IRH/Commands/SetupDeployment/SetupDeployment.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using IRH.Commands.SetupDeployment.Types;
+using Serilog.Core;
 using System.CommandLine;
 
 namespace IRH.Commands.SetupDeployment
