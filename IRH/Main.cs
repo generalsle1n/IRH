6c589aae6e861e55b65ea86155e8a0f8a32fed13
Niels Schuler <niels.schuler@web.de>
2024-07-28 18:46:04 +0200

    Added Command

--- IRH/Main.cs
+++ IRH/Main.cs
@@ -13,11 +13,14 @@ RootCommand RootCommand = new RootCommand();
 
 LDAPMonitor LM = new LDAPMonitor(Logger);
 SetupDeployment SD = new SetupDeployment(Logger);
+AzureMFA AMFA = new AzureMFA(Logger);
 
 Command LdapMonitor = LM.CreateCommand(RootCommand);
 Command SetupDeployment = SD.CreateCommand(RootCommand);
+Command AzureMFA = AMFA.CreateCommand(RootCommand);
 
 RootCommand.AddCommand(LdapMonitor);
 RootCommand.AddCommand(SetupDeployment);
+RootCommand.AddCommand(AzureMFA);
 
 await RootCommand.InvokeAsync(args);
