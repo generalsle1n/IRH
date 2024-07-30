abe738e3d73a343df3664f1651ead752860a2794
Niels Schuler <niels.schuler@web.de>
2024-07-30 21:01:12 +0200

    Added Print Levels

---
+++ IRH/Commands/AzureMFA/Reporting/ReportPrintLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.AzureMFA.Reporting
+{
+    internal enum ReportPrintLevel
+    {
+        Brief,
+        Info,
+        Detailed,
+        Hacky
+    }
+}
