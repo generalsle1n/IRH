0527fed0a02f16d973c847e27e7433c80bc3ec2d
Niels Schuler <niels.schuler@web.de>
2024-07-30 17:45:06 +0200

    Added Options Const

--- IRH/Commands/AzureMFA/AzureMFA.cs
+++ IRH/Commands/AzureMFA/AzureMFA.cs
@@ -12,6 +12,21 @@ namespace IRH.Commands.LDAPMonitor
         private const string _filterOnGroupDescription = "Enter the ID for the Group or multiple seperated by comma";
         private const string _filterOnGroupAlias = "--Group";
 
+        private const string _permissionScopes = "-P";
+        private const string _permissionScopesDescription = "Enter the custom permission to access the api";
+        private const string _permissionScopesAlias = "--PermissionScope";
+        private string[] _permissionScopesDefaultValue = new string[] { "Directory.Read.All", "UserAuthenticationMethod.Read.All" };
+
+        private const string _publicAppID = "-A";
+        private const string _publicAppIDDescription = "Enter the ID of the App ID";
+        private const string _publicAppIDAlias = "--AppID";
+        private const bool _publicAppIDIsRequired = true;
+
+        private const string _publicTenantID = "-T";
+        private const string _publicTenantIDDescription = "Enter the ID of the Tenant ID (In the default you dont need to change this)";
+        private const string _publicTenantIDAlias = "--Tenant";
+        private const string _publicTenantIDDefaultValue = "common";
+
         private readonly Logger _logger;
 
         internal AzureMFA(Logger Logger)
