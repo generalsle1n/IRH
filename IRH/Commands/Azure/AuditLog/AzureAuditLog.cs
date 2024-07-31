ff53c9a3ba030f988ece710cf3b92b1f33ec289c
Niels Schuler <niels.schuler@web.de>
2024-07-31 22:10:21 +0200

    Added Option Value for CLI

--- IRH/Commands/Azure/AuditLog/AzureAuditLog.cs
+++ IRH/Commands/Azure/AuditLog/AzureAuditLog.cs
@@ -23,6 +23,38 @@ namespace IRH.Commands.Azure.AuditLog
         private const string _publicTenantIDAlias = "--Tenant";
         private const string _publicTenantIDDefaultValue = "common";
 
+        private const string _startDate = "-S";
+        private const string _startDateDescription = "Enter the Start of the Investigation (Just in Format DD.MM.YYYY)";
+        private const string _startDateAlias = "--Start";
+        private const bool _startDateIsRequired = true;
+
+        private const string _endDate = "-E";
+        private const string _endDateDescription = "Enter the End of the Investigation (Just in Format DD.MM.YYYY)";
+        private const string _endDateAlias = "--End";
+        private const bool _endDateIsRequired = true;
+
+        private const string _defaultActivities = "-AC";
+        private const string _defaultActivitiesDescription = "Enter the Default Activities that should be searched in the Audit Logs (Seperated By Whitespace)";
+        private const string _defaultActivitiesAlias = "--Activities";
+        private string[] _defaultActivitiesDefaultValue = new string[] { "New-TransportRule", "New-InboxRule" };
+
+        private const string _waitQueryTime = "-QT";
+        private const string _waitQueryTimeDescription = "Enter the Value how long to wait between the single query checks (In Seconds)";
+        private const string _waitQueryTimeAlias = "--QueryWait";
+        private const int _waitQueryTimeDefaultValue = 10;
+
+        private const string _reportType = "-R";
+        private const string _reportTypeDescription = "How to Report the Data";
+        private const string _reportTypeAlias = "--Report";
+        private const ReportType _reportTypeDefaultValue = ReportType.CLI;
+
+        private const string _printLevel = "-PL";
+        private const string _printLevelDescription = "How detailed to be printed";
+        private const string _printLevelAlias = "--PrintLevel";
+        private const ReportPrintLevel _printLevelDefaultValue = ReportPrintLevel.Brief;
+
+        private const int _timeMultiplyer = 1000;
+
         private readonly Logger _logger;
 
         internal AzureAuditLog(Logger Logger)
