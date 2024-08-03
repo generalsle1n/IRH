e0ce7fa2b596f788e0b2ab829e0a33f511ac766a
Niels Schuler <niels.schuler@web.de>
2024-08-03 19:31:16 +0200

    Renamed Namespaces

--- IRH/Commands/Azure/Reporting/ReportType.cs
+++ IRH/Commands/Azure/Reporting/ReportType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IRH.Commands.AzureMFA.Reporting
+namespace IRH.Commands.Azure.Reporting
 {
     internal enum ReportType
     {
