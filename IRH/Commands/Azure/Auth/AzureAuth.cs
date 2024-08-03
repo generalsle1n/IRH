d461c6f8ee86abb64b331ed02f4aeedc273511a8
Niels Schuler <niels.schuler@web.de>
2024-08-03 21:11:49 +0200

    Added Functionalty to decide on the auth Type

--- IRH/Commands/Azure/Auth/AzureAuth.cs
+++ IRH/Commands/Azure/Auth/AzureAuth.cs
@@ -6,13 +6,49 @@ namespace IRH.Commands.Azure.Auth
 {
     internal class AzureAuth
     {
-        internal GraphServiceClient GetDeviceClient(string AppIDValue, string TenantIDValue, string[] ScopesValue)
+        internal GraphServiceClient GetClient(string AppIDValue, string TenantIDValue, string[] ScopesValue, AuthType Type)
+        {
+            GraphServiceClient Client = null;
+            switch (Type)
+            {
+                case AuthType.DeviceCode:
+                    DeviceCodeCredential DeviceCredentials = CreateDeviceCodeCredential(AppIDValue, TenantIDValue);
+                    Client = new GraphServiceClient(DeviceCredentials, ScopesValue);
+                    break;
+                case AuthType.Interactive:
+                    InteractiveBrowserCredential InteractiveCredentials = CreateInteractiveBrowserCredential(AppIDValue, TenantIDValue);
+                    Client = new GraphServiceClient(InteractiveCredentials, ScopesValue);
+                    break;
+            }
+
+            return Client;
+        }
+        internal BGraphServiceClient GetClientBeta(string AppIDValue, string TenantIDValue, string[] ScopesValue, AuthType Type)
+        {
+            BGraphServiceClient Client = null;
+
+            switch (Type)
+            {
+                case AuthType.DeviceCode:
+                    DeviceCodeCredential DeviceCredentials = CreateDeviceCodeCredential(AppIDValue, TenantIDValue);
+                    Client = new BGraphServiceClient(DeviceCredentials, ScopesValue);
+                    break;
+                case AuthType.Interactive:
+                    InteractiveBrowserCredential InteractiveCredentials = CreateInteractiveBrowserCredential(AppIDValue, TenantIDValue);
+                    Client = new BGraphServiceClient(InteractiveCredentials, ScopesValue);
+                    break;
+            }
+
+            return Client;
+        }
+
+        private DeviceCodeCredential CreateDeviceCodeCredential(string AppID, string TenantID)
         {
             DeviceCodeCredentialOptions Options = new DeviceCodeCredentialOptions
             {
                 AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-                ClientId = AppIDValue,
-                TenantId = TenantIDValue,
+                ClientId = AppID,
+                TenantId = TenantID,
 
                 DeviceCodeCallback = (code, cancellation) =>
                 {
@@ -21,31 +57,23 @@ namespace IRH.Commands.Azure.Auth
                 },
             };
 
-            DeviceCodeCredential Credentials = new DeviceCodeCredential(Options);
-            GraphServiceClient Client = new GraphServiceClient(Credentials, ScopesValue);
-
-            return Client;
+            return new DeviceCodeCredential(Options);
         }
-        internal BGraphServiceClient GetDeviceClientBeta(string AppIDValue, string TenantIDValue, string[] ScopesValue)
-        {
-            DeviceCodeCredentialOptions Options = new DeviceCodeCredentialOptions
-            {
-                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-                ClientId = AppIDValue,
-                TenantId = TenantIDValue,
 
-                DeviceCodeCallback = (code, cancellation) =>
-                {
-                    Console.WriteLine(code.Message);
-                    return Task.FromResult(0);
-                },
+        private InteractiveBrowserCredential CreateInteractiveBrowserCredential(string AppID, string TenantID)
+        {
+            InteractiveBrowserCredentialOptions Options = new InteractiveBrowserCredentialOptions
+            {
+                TenantId = TenantID,
+                ClientId = AppID,
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+                // MUST be http://localhost or http://localhost:PORT
+                // See https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/System-Browser-on-.Net-Core
+                RedirectUri = new Uri("http://localhost"),
             };
 
-            DeviceCodeCredential Credentials = new DeviceCodeCredential(Options);
-            BGraphServiceClient Client = new BGraphServiceClient(Credentials, ScopesValue);
-
-            return Client;
+            // https://learn.microsoft.com/dotnet/api/azure.identity.interactivebrowsercredential
+            return new InteractiveBrowserCredential(Options);
         }
-
     }
 }
