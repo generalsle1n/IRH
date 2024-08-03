e3ada8d4094142dfb0870bd0d7de7736006a3ad0
Niels Schuler <niels.schuler@web.de>
2024-08-03 21:32:56 +0200

    Added Option for Filtering

--- IRH/Commands/Azure/AuditLog/AzureAuditLog.cs
+++ IRH/Commands/Azure/AuditLog/AzureAuditLog.cs
@@ -10,6 +10,10 @@ namespace IRH.Commands.Azure.AuditLog
         private const string _commandName = "-Audit";
         private const string _commandDescription = "Operate with the Audit System from Microsoft";
 
+        private const string _filterOnAttributes = "-F";
+        private const string _filterOnAttributesDescription = "Filter on Parameternames, Wildcards are supported (This Setting works only on Printlevel Info and above)";
+        private const string _filterOnAttributesAlias = "--Filter";
+
         private readonly Logger _logger;
 
         internal AzureAuditLog(Logger Logger)
@@ -20,6 +24,11 @@ namespace IRH.Commands.Azure.AuditLog
         internal Command CreateCommand(RootCommand RootCommand)
         {
             Command Command = new Command(name: _commandName, description: _commandDescription);
+
+            Option<string[]> FilterOnParameter = new Option<string[]>(name: _filterOnAttributes, description: _filterOnAttributesDescription);
+            FilterOnParameter.AddAlias(_filterOnAttributesAlias);
+
+            Command.AddGlobalOption(FilterOnParameter);
 
             ExchangeAudit ExchangeAuditCommand = new ExchangeAudit(_logger);
             Command ExchangeCommand = ExchangeAuditCommand.CreateCommand(RootCommand);
