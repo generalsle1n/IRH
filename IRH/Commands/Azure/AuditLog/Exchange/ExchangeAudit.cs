fa9fcfd7ec2e99ebe0f3fef6080c45aab23cfb75
Niels Schuler <niels.schuler@web.de>
2024-08-03 20:34:34 +0200

    FileType

--- IRH/Commands/Azure/AuditLog/Exchange/ExchangeAudit.cs
+++ IRH/Commands/Azure/AuditLog/Exchange/ExchangeAudit.cs
@@ -120,7 +120,7 @@ namespace IRH.Commands.Azure.AuditLog.Exchange
 
                 AzureAuth Auth = new AzureAuth();
                 AuditHelper Helper = new AuditHelper(_logger);
-                
+            
                 GraphServiceClient Client = Auth.GetDeviceClientBeta(
                     Parser.GetValueForOption(AppID),
                     Parser.GetValueForOption(TenantID),
