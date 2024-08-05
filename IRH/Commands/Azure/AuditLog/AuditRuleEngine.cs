0a7d76d3819fd6c7c94b033afdbf50a230edc69f
Niels Schuler <niels.schuler@web.de>
2024-08-05 20:28:47 +0200

    Required Usings

--- IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
+++ IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Graph.Beta.Models.Security;
+using Serilog.Core;
 
 namespace IRH.Commands.Azure
 {
