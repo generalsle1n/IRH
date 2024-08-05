544b1a243ff73619724b9b5a0a853469fd1268ce
Niels Schuler <niels.schuler@web.de>
2024-08-05 20:28:06 +0200

    Added Method to Process an Object with the Defined Rules

--- IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
+++ IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
@@ -8,5 +8,41 @@ namespace IRH.Commands.Azure
 {
     internal class AuditRuleEngine
     {
+        internal async Task<bool> ProcessAudit(AuditLogRecord Record)
+        {
+            PropertyInfo[] AllProperties = Record.GetType().GetProperties();
+
+            List<bool> AllResult = new List<bool>();
+
+            foreach(KeyValuePair<string, string> SingleRule in _rules)
+            {
+                bool SingleResult = false;
+
+                IEnumerable<PropertyInfo> Search = AllProperties.Where(filter => filter.Name.Equals(SingleRule.Key));
+
+                _logger.Verbose($"Evaluted {AllProperties.Length} Properties for Object {Record.Id}");
+
+                if(Search.Count() > 0)
+                {
+                    PropertyInfo FilterdProperty = Search.First();
+                    string PropertyValue = FilterdProperty.GetValue(Record) as string;
+
+                    _logger.Verbose($"Searching {FilterdProperty.Name} in {Record.Id}");
+
+                    if (PropertyValue.Equals(SingleRule.Value))
+                    {
+                        SingleResult = true;
+                    }
+
+                    _logger.Verbose($"Match for {FilterdProperty.Name}: {SingleResult}");
+                }
+
+                AllResult.Add(SingleResult);
+            }
+
+            bool Result = AllResult.All(filter => filter == true);
+
+            return Result;
+        }
     }
 }
