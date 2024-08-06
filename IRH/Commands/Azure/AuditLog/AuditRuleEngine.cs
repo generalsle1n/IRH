0a6644dcc043eaaf2167a8df9f85f3a248f22b20
Niels Schuler <niels.schuler@web.de>
2024-08-06 19:19:44 +0200

    Moved to AuditProcessorRules

--- IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
+++ IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using IRH.Commands.Azure.AuditLog.Model;
 using IRH.Commands.Azure.Helper;
 using Microsoft.Graph.Beta.Models.Security;
 using Serilog.Core;
@@ -12,19 +14,33 @@ namespace IRH.Commands.Azure.AuditLog
 {
     internal class AuditRuleEngine
     {
-        private List<KeyValuePair<string, string>> _rules;
+        private List<AuditProcessorRule> _rules;
         private readonly Logger _logger;
         private const string _ruleSeperator = ":";
 
         internal AuditRuleEngine(string[] RuleText, Logger Logger)
         {
             _logger = Logger;
-            _rules = new List<KeyValuePair<string, string>>();
+
+            _rules = new List<AuditProcessorRule>();
 
             foreach (string SingleRule in RuleText)
             {
                 string[] Splitted = SingleRule.Split(_ruleSeperator);
-                _rules.Add(new KeyValuePair<string, string>(Splitted[0], Splitted[1]));
+
+                _rules.Add(new AuditProcessorRule()
+                {
+                    RawRule = new AuditRawRule()
+                    {
+                        ParamterNameFilter = Splitted[0],
+                        ParamterValueFilter = Splitted[1]
+                    },
+                    RegexRule = new AuditRegexRule()
+                    {
+                        ParamterNameFilter = CreateRegex(Splitted[0]),
+                        ParamterValueFilter = CreateRegex(Splitted[1])
+                    }
+                });
 
                 _logger.Information($"Loaded {SingleRule} Rule into Engine");
             }
@@ -38,11 +54,11 @@ namespace IRH.Commands.Azure.AuditLog
 
             List<bool> AllResult = new List<bool>();
 
-            foreach (KeyValuePair<string, string> SingleRule in _rules)
+            foreach (AuditProcessorRule SingleRule in _rules)
             {
                 bool SingleResult = false;
 
-                IEnumerable<PropertyInfo> Search = AllProperties.Where(filter => filter.Name.Equals(SingleRule.Key));
+                IEnumerable<PropertyInfo> Search = AllProperties.Where(filter => filter.Name.Equals(SingleRule.RawRule.ParamterNameFilter));
 
                 _logger.Verbose($"Evaluted {AllProperties.Length} Properties for Object {Record.Id}");
 
@@ -53,7 +69,7 @@ namespace IRH.Commands.Azure.AuditLog
 
                     _logger.Verbose($"Searching {FilterdProperty.Name} in {Record.Id}");
 
-                    if (PropertyValue.Equals(SingleRule.Value))
+                    if (PropertyValue.Equals(SingleRule.RawRule.ParamterValueFilter))
                     {
                         SingleResult = true;
                     }
@@ -62,25 +78,25 @@ namespace IRH.Commands.Azure.AuditLog
                 }
                 else
                 {
-                    IEnumerable<KeyValuePair<string, object>> SearchResult = Record.AuditData.AdditionalData.Where(item => item.Key.Equals(SingleRule.Key));
+                    IEnumerable<KeyValuePair<string, object>> SearchResult = Record.AuditData.AdditionalData.Where(item => item.Key.Equals(SingleRule.RawRule.ParamterNameFilter));
 
                     if (SearchResult.Count() > 0)
                     {
-                        SingleResult = SingleRule.Value.Equals(SearchResult.First().Value.ToString());
+                        SingleResult = SingleRule.RawRule.ParamterValueFilter.Equals(SearchResult.First().Value.ToString());
                     }
                     else
                     {
                         _logger.Verbose($"Try to expand object{Record.Id} for further search");
                         List<KeyValuePair<string, string>> Expanded = await UnTypedExtractor.ExtractUntypedDataFromAuditLogRecord(Record);
 
-                        IEnumerable<KeyValuePair<string, string>> SearchForExpand = Expanded.Where(filter => filter.Key.Equals(SingleRule.Key) && filter.Value.Equals(SingleRule.Value));
+                        IEnumerable<KeyValuePair<string, string>> SearchForExpand = Expanded.Where(filter => filter.Key.Equals(SingleRule.RawRule.ParamterNameFilter) && filter.Value.Equals(SingleRule.RawRule.ParamterValueFilter));
 
                         if (SearchForExpand.Count() > 0)
                         {
                             SingleResult = true;
                         }
 
-                        _logger.Verbose($"Match for Expanded {SingleRule.Key}: {SingleResult}");
+                        _logger.Verbose($"Match for Expanded {SingleRule.RawRule.ParamterNameFilter}: {SingleResult}");
                     }
                 }
 
@@ -91,6 +107,12 @@ namespace IRH.Commands.Azure.AuditLog
             bool Result = AllResult.All(filter => filter == true);
 
             return Result;
+        }
+
+        internal Regex CreateRegex(string Input)
+        {
+            string CurrentPattern = $"^{Input.Replace("*", ".*")}$";
+            return new Regex(CurrentPattern, RegexOptions.IgnoreCase);
         }
     }
 }
