1aa5c3aff4f8afc59343caab2e4ac4f71dbb2bba
Niels Schuler <niels.schuler@web.de>
2024-08-06 19:00:05 +0200

    Added Model for Rules

---
+++ IRH/Commands/Azure/AuditLog/Model/AuditProcessorRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.Azure.AuditLog.Model
+{
+    internal class AuditProcessorRule
+    {
+        internal AuditRawRule RawRule { get; set; }
+    }
+}
---
+++ IRH/Commands/Azure/AuditLog/Model/AuditRegexRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IRH.Commands.Azure.AuditLog.Model
+{
+    internal class AuditRegexRule
+    {
+        internal Regex ParamterNameFilter { get; set; }
+        internal Regex ParamterValueFilter { get; set; }
+    }
+}
