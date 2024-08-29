519d59f36ffe2bccf1ae6259d622317ec41e07d2
Niels Schuler <niels.schuler@web.de>
2024-08-29 17:48:23 +0200

    Added Method

--- IRH.ProcessElevation/ProcessElevation.cs
+++ IRH.ProcessElevation/ProcessElevation.cs
@@ -1,7 +1,12 @@
-﻿namespace IRH.ProcessElevation
+﻿using IRH.ProcessElevation.Model;
+
+namespace IRH.ProcessElevation
 {
     public class ProcessElevation
     {
+        public void CreateProcess(DestinationType Type)
+        {
 
+        }
     }
 }
