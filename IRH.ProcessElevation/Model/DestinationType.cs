cd336cb8dd2237d9cbaf72e903c5bba5cfb7276a
Niels Schuler <niels.schuler@web.de>
2024-09-01 13:15:25 +0200

    Changed Access Type

--- IRH.ProcessElevation/Model/DestinationType.cs
+++ IRH.ProcessElevation/Model/DestinationType.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IRH.ProcessElevation.Model
 {
-    internal enum DestinationType
+    public enum DestinationType
     {
         Domain,
         Workgroup
