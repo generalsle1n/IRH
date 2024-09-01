f6c9ecc7dc249332b94ce192cb492d9514b1440a
Niels Schuler <niels.schuler@web.de>
2024-09-01 12:37:39 +0200

    Added new Types

---
+++ IRH.ProcessElevation/Model/SecurityBuffer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.ProcessElevation.Model
+{
+    internal class SecurityBuffer : IDisposable
+    {
+        public int Buffer;
+        public int BufferType;
+        public IntPtr BufferPointer;
+
+
+        public SecurityBuffer(int BufferSize)
+        {
+            Buffer = BufferSize;
+            BufferType = (int)SecurityBufferType.Token;
+            BufferPointer = Marshal.AllocHGlobal(BufferSize);
+        }
+
+        public SecurityBuffer(byte[] SecurityBufferBytes)
+        {
+            Buffer = SecurityBufferBytes.Length;
+            BufferType = (int)SecurityBufferType.Token;
+            BufferPointer = Marshal.AllocHGlobal(BufferPointer);
+            Marshal.Copy(SecurityBufferBytes, 0, BufferPointer, Buffer);
+        }
+
+        public SecurityBuffer(byte[] SecurityBufferBytes, SecurityBufferType Type)
+        {
+            Buffer = SecurityBufferBytes.Length;
+            BufferType = (int)Type;
+            BufferPointer = Marshal.AllocHGlobal(Buffer);
+            Marshal.Copy(SecurityBufferBytes, 0, BufferPointer, Buffer);
+        }
+
+        public void Dispose()
+        {
+            if (BufferPointer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(BufferPointer);
+                BufferPointer = IntPtr.Zero;
+            }
+        }
+    }
+}
---
+++ IRH.ProcessElevation/Model/SecurityBufferType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.ProcessElevation.Model
+{
+    internal enum SecurityBufferType
+    {
+        Version = 0,
+        Empty= 0,
+        Data= 1,
+        Token= 2
+    }
+}
