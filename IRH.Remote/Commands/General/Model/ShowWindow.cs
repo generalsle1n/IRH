d84c47e92887648dd94f97e81de6d4d67bb9d7c0
Niels Schuler <niels.schuler@web.de>
2025-01-06 10:29:47 +0100

    Added Static Model

---
+++ IRH.Remote/Commands/General/Model/ShowWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Remote.Commands.General.Model
+{
+    internal enum ShowWindow
+    {
+        SW_HIDE = 0,
+        SW_NORMAL = 1,
+        SW_SHOWMINIMIZED = 2,
+        SW_MAXIMIZE = 3,
+        SW_SHOWNOACTIVATE = 4,
+        SW_SHOW = 5,
+        SW_MINIMIZE = 6,
+        SW_SHOWMINNOACTIVE = 7,
+        SW_SHOWNA = 8,
+        SW_RESTORE = 9,
+        SW_SHOWDEFAULT = 10,
+        SW_FORCEMINIMIZE = 11
+    }
+}
