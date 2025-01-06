e02b12fc051921dc215aae763ead37b580ad90d3
Niels Schuler <niels.schuler@web.de>
2025-01-06 11:24:46 +0100

    Added new File

---
+++ IRH.Remote/Commands/ExecuteFile/ExecuteFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Remote.Commands.ExecuteFile
+{
+    internal class ExecuteFile
+    {
+        private void a()
+        {
+            UploadFile.UploadFile uploadFile = new UploadFile.UploadFile(null);
+            uploadFile.
+        }
+    }
+}
