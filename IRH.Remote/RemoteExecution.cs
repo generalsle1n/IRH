0096c1d9a1fbd0486c80a2f3fbfbdbef670bebbf
Niels Schuler <niels.schuler@web.de>
2025-01-06 10:29:31 +0100

    Added Remote Command

---
+++ IRH.Remote/RemoteExecution.cs
@@ -0,0 +1,32 @@
+﻿using Serilog.Core;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using IRH.Remote.Commands.UploadFile;
+
+namespace IRH.Remote
+{
+    public class RemoteExecution
+    {
+        private const string _commandName = "-R";
+        private const string _commandDescription = "All available Remote Commands";
+
+        private readonly Logger _logger;
+
+        public RemoteExecution(Logger Logger)
+        {
+            _logger = Logger;
+        }
+
+        public Command CreateCommand(RootCommand Root)
+        {
+            Command Command = new Command(name: _commandName, description: _commandDescription);
+
+            UploadFile UploadFile = new UploadFile(_logger);
+            Command ExecuteFileCommand = UploadFile.CreateCommand(Command);
+
+            Command.AddCommand(ExecuteFileCommand);
+
+            return Command;
+        }
+    }
+}
--- IRH/Main.cs
+++ IRH/Main.cs
@@ -1,11 +1,11 @@
 ﻿using IRH.Commands.Azure.MFA;
 using IRH.Commands.Azure.AuditLog;
 using IRH.Commands.LDAPMonitor;
-using IRH.Commands.SetupDeployment;
 using Serilog;
 using Serilog.Core;
 using System.CommandLine;
 using IRH.Commands.Azure;
+using IRH.Remote;
 
 const string _commandDescription = "suite of some little helper tools within incident response when dealing with security breaches. These tools provide essential features for IT security professionals, making it easier to manage and respond to incidents effectively.";
 
@@ -17,15 +17,15 @@ Logger Logger = new LoggerConfiguration()
 RootCommand RootCommand = new RootCommand(_commandDescription);
 
 LDAPMonitor LM = new LDAPMonitor(Logger);
-SetupDeployment SD = new SetupDeployment(Logger);
+RemoteExecution RM = new RemoteExecution(Logger);
 AzureFunctions AF = new AzureFunctions(Logger);
 
 Command LdapMonitor = LM.CreateCommand(RootCommand);
-Command SetupDeployment = SD.CreateCommand(RootCommand);
+Command RemoteExecution = RM.CreateCommand(RootCommand);
 Command AzureFunctions = AF.CreateCommand(RootCommand);
 
 RootCommand.AddCommand(LdapMonitor);
-RootCommand.AddCommand(SetupDeployment);
+RootCommand.AddCommand(RemoteExecution);
 RootCommand.AddCommand(AzureFunctions);
 
 await RootCommand.InvokeAsync(args);
