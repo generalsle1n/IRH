e8c4fb3f80ebea1b7fa6aa4c3e855dc12b38fd0a
Niels Schuler <niels.schuler@web.de>
2025-06-10 16:40:30 +0200

    Added Model for User Session revokaton

---
+++ IRH/Commands/Azure/Reporting/Model/UserSession.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Graph.Models;
+using Microsoft.Graph.Users.Item.RevokeSignInSessions;
+
+namespace IRH.Commands.Azure.Reporting.Model
+{
+    public class UserSession
+    {
+        public User User { get; set; }
+        public bool ResetToken {  get; set; }
+        public RevokeSignInSessionsPostResponse Response { get; set; }
+    }
+}
