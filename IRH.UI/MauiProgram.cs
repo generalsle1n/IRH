eddb991b83dce4f0d1e1329e1d7aea1f10e3f89f
Niels Schuler <niels.schuler@web.de>
2025-06-11 11:42:54 +0200

    Added Loggin

--- IRH.UI/MauiProgram.cs
+++ IRH.UI/MauiProgram.cs
@@ -1,11 +1,16 @@
 ﻿using CommunityToolkit.Maui;
 using IRH.UI.Custom;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Reflection;
+using ILogger = Serilog.ILogger;
 
 namespace IRH.UI
 {
     public static class MauiProgram
     {
+        private const string _logFolderName = "Logs";
+        private const string _logFileName = "log.txt";
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -20,6 +25,25 @@ namespace IRH.UI
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
+            
+            Assembly Current = Assembly.GetExecutingAssembly();
+
+            string Location = Path.GetDirectoryName(Current.Location);
+            string AbsoluteFolderPath = Path.Combine(Location, _logFolderName);
+
+            if (!Path.Exists(AbsoluteFolderPath))
+            {
+                Directory.CreateDirectory(AbsoluteFolderPath);
+            }
+
+            string AbsoluteLogPath = Path.Combine(AbsoluteFolderPath, _logFileName);
+
+            ILogger logger = new LoggerConfiguration()
+                    .WriteTo.File(AbsoluteLogPath)
+                    .MinimumLevel.Verbose()
+                    .CreateLogger();
+
+            builder.Services.AddSingleton<ILogger>(logger);
 
             PreferenceHelper.LoadInitData();
 
