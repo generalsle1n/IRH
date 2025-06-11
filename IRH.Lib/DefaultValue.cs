709ef6bf657707a7ae10ee5279e17ae981576a24
Niels Schuler <niels.schuler@web.de>
2025-06-11 08:45:39 +0200

    Added Lib to Reference

---
+++ IRH.Lib/DefaultValue.cs
@@ -0,0 +1,19 @@
+﻿using IRH.Lib.Model.Azure.Auth;
+using IRH.Lib.Model.Azure.Reporting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Lib
+{
+    public class DefaultValue
+    {
+        public const string TenantID = "common";
+        public const string AppID = "c0849608-c8b9-4e86-b37d-fce972a0a7f6";
+        public readonly static AuthType AuthType = AuthType.DeviceCode;
+        public readonly static List<string> AzureMfaPermissions = new List<string>() { "Directory.Read.All", "UserAuthenticationMethod.Read.All" };
+        public readonly static ReportType ReportType = ReportType.CLI;
+    }
+}
---
+++ IRH.Lib/Model/Azure/Auth/AuthType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Lib.Model.Azure.Auth
+{
+    public enum AuthType
+    {
+        DeviceCode,
+        Interactive
+    }
+}
