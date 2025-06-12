6e9d02931378fbe0a99bff9dc19a3ea3818c8978
Niels Schuler <niels.schuler@web.de>
2025-06-12 22:48:48 +0200

    Added `AzureMFAItemControlTemplate` model in `IRH.UI.Models.Azure`.

---
+++ IRH.UI/Models/Azure/AzureMFAItemControlTemplate.cs
@@ -0,0 +1,12 @@
+﻿namespace IRH.Ui.Models.Azure;
+
+public class AzureMFAItemControlTemplate
+{
+    public AzureMFAItemControlTemplate(string label, bool showDelete=true)
+    {
+        Label = label;
+        ShowDelete = showDelete;
+    }
+    public string Label { get; }
+    public bool ShowDelete { get; }
+}
