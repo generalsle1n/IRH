42c5930854e7e5f59f899aa0d3d989a99492a59c
Niels Schuler <niels.schuler@web.de>
2025-06-12 22:49:57 +0200

    Changed `readonly static` to `static readonly` for consistency in `DefaultValue` constants.

--- IRH.Lib/DefaultValue.cs
+++ IRH.Lib/DefaultValue.cs
@@ -12,9 +12,9 @@ namespace IRH.Lib
     {
         public const string TenantID = "common";
         public const string AppID = "c0849608-c8b9-4e86-b37d-fce972a0a7f6";
-        public readonly static AuthType AuthType = AuthType.DeviceCode;
-        public readonly static List<string> AzureMfaPermissions = new List<string>() { "Directory.Read.All", "UserAuthenticationMethod.Read.All" };
-        public readonly static ReportType ReportType = ReportType.CLI;
-        public readonly static ReportPrintLevel PrintLevel = ReportPrintLevel.Brief;
+        public static readonly AuthType AuthType = AuthType.DeviceCode;
+        public static readonly List<string> AzureMfaPermissions = new List<string>() { "Directory.Read.All", "UserAuthenticationMethod.Read.All" };
+        public static readonly ReportType ReportType = ReportType.CLI;
+        public static readonly ReportPrintLevel PrintLevel = ReportPrintLevel.Brief;
     }
 }
