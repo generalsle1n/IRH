dc4237a5df47dfe0a5501881ecb7a3bec32f2af5
Niels Schuler <niels.schuler@web.de>
2025-06-12 17:24:40 +0200

    Removed not required Usings

--- IRH.Ui/Views/MainWindow.axaml.cs
+++ IRH.Ui/Views/MainWindow.axaml.cs
@@ -4,9 +4,6 @@ using Avalonia.Media;
 using FluentAvalonia.UI.Controls;
 using FluentAvalonia.UI.Windowing;
 
-using SymbolIconSource = FluentIcons.Avalonia.Fluent.SymbolIconSource;
-using Symbol = FluentIcons.Common.Symbol;
-
 namespace IRH.Ui.Views
 {
     public partial class MainWindow : AppWindow
