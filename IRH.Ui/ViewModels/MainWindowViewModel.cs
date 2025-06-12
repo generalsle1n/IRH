affa138335bbb7936aeaf97caa597082c52d8f2d
Niels Schuler <niels.schuler@web.de>
2025-06-12 17:23:41 +0200

    Added Support for Context Support

--- IRH.Ui/ViewModels/MainWindowViewModel.cs
+++ IRH.Ui/ViewModels/MainWindowViewModel.cs
@@ -1,39 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Reflection;
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using FluentAvalonia.UI.Controls;
-using IRH.Ui.Models.Generel;
-// using IRH.Ui.Models.Generel;
 using IRH.Ui.Views;
-using Symbol = FluentIcons.Common.Symbol;
 
 namespace IRH.Ui.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private UserControl _currentPage = new SettingView();
+        private UserControl _currentPage = new SettingView()
+        {
+            DataContext = new SettingViewModel()
+        };
 
         [ObservableProperty]
         private NavigationViewItem _selectedItem;
 
         partial void OnSelectedItemChanged(NavigationViewItem value)
         {
+            string[] SplitType = (value.Tag as string).Split(";");
             
-            string TypeName = value.Tag as string;
-            Type SingleType = Type.GetType(TypeName);
-
-            if (SingleType is null)
-            {
-                SingleType = typeof(SettingView);
-            }
+            string ViewTypeName = SplitType[0];
+            string ViewModelTypeName = SplitType[1];
             
-            UserControl SingleView = (UserControl)Activator.CreateInstance(SingleType);
-
+            Type ViewType = Type.GetType(ViewTypeName) ?? typeof(SettingView);
+            Type ViewModelType = Type.GetType(ViewTypeName) ?? typeof(SettingViewModel);
+            
+            UserControl SingleView = (UserControl)Activator.CreateInstance(ViewType);
+            ViewModelBase SingleViewModel = (ViewModelBase)Activator.CreateInstance(ViewModelType);
+            
+            SingleView.DataContext = SingleViewModel;
+            
             CurrentPage = SingleView;
             SelectedItem = value;
         }
