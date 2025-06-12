41f96762c3202dca9080f9488aae3d7896c255f2
Niels Schuler <niels.schuler@web.de>
2025-06-12 22:50:06 +0200

    Added commands and observable collections for managing group filters and scopes in `AzureMFAViewModel`.

--- IRH.Ui/ViewModels/AzureMFAViewModel.cs
+++ IRH.Ui/ViewModels/AzureMFAViewModel.cs
@@ -1,5 +1,60 @@
-﻿namespace IRH.Ui.ViewModels;
-public class AzureMFAViewModel : ViewModelBase
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.SimplePreferences;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IRH.Lib;
+using IRH.Lib.Model.Azure.Reporting;
+using IRH.Ui.Models.Azure;
+using IRH.Ui.Models.UI;
+using Strings = IRH.Ui.Resources.Strings;
+
+namespace IRH.Ui.ViewModels;
+public partial class AzureMFAViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private ReportPrintLevel _selectedReportLevel = DefaultValue.PrintLevel;
     
+    public ObservableCollection<AzureMFAItemControlTemplate> AllGroupFilter { get; }= new ObservableCollection<AzureMFAItemControlTemplate>()
+    {
+        new AzureMFAItemControlTemplate(null, showDelete:false)
+    };
+    
+    public ObservableCollection<AzureMFAItemControlTemplate> AllScopes { get; }= new ObservableCollection<AzureMFAItemControlTemplate>(
+        DefaultValue.AzureMfaPermissions.Select((singleString, index) => new AzureMFAItemControlTemplate(singleString,showDelete:index != 0))
+    );
+    
+    internal List<ReportPrintLevel> AllReportLevel { get; } = Enum.GetValues<ReportPrintLevel>().Cast<ReportPrintLevel>().ToList();
+
+    [RelayCommand]
+    private async Task AddNewGroupFilter()
+    {
+        AllGroupFilter.Add(new AzureMFAItemControlTemplate(null));
+    }
+    
+    [RelayCommand]
+    private async Task DeleteGroupFilter(object Sender)
+    {
+        Button SingleButton = Sender as Button;
+        AzureMFAItemControlTemplate Item = SingleButton.DataContext as AzureMFAItemControlTemplate;
+        AllGroupFilter.Remove(Item);
+    }
+    
+    [RelayCommand]
+    private async Task AddNewScope()
+    {
+        AllScopes.Add(new AzureMFAItemControlTemplate(null));
+    }
+    
+    [RelayCommand]
+    private async Task DeleteScope(object Sender)
+    {
+        Button SingleButton = Sender as Button;
+        AzureMFAItemControlTemplate Item = SingleButton.DataContext as AzureMFAItemControlTemplate;
+        AllScopes.Remove(Item);
+    }
 }
