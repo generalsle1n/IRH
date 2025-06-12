f86530a1d365511c4d62191a19397d1ff10c554c
Niels Schuler <niels.schuler@web.de>
2025-06-12 17:23:52 +0200

    Added ViewModel for Settings

---
+++ IRH.Ui/ViewModels/SettingViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using IRH.Lib.Model.Azure.Auth;
+using IRH.Ui.Models.UI;
+
+namespace IRH.Ui.ViewModels;
+
+internal partial class SettingViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private AppTheme _selectedAppTheme;
+    [ObservableProperty]
+    private AuthType _selectedAuthType;
+    [ObservableProperty]
+    private string _currentTenantID;
+    [ObservableProperty]
+    private string _currentAppID;
+    [ObservableProperty]
+    private bool _tenantIDEditEnabled = false;
+    [ObservableProperty]
+    private bool _appIDEditEnabled = false;
+    
+    internal List<AppTheme> AllAppThemes { get; } = Enum.GetValues<AppTheme>().Cast<AppTheme>().ToList();
+    public List<AuthType> AllAuthTypes { get; } = Enum.GetValues<AuthType>().Cast<AuthType>().ToList();
+}
