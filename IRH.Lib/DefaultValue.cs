2049ca2683f29454259420a2d9ec157b0962f545
Niels Schuler <niels.schuler@web.de>
2025-06-13 14:29:09 +0200

    Added `DeviceLoginUrl` constant to `DefaultValue` for Azure device login URI.

--- IRH.Lib/DefaultValue.cs
+++ IRH.Lib/DefaultValue.cs
@@ -14,6 +14,7 @@ namespace IRH.Lib
         public const string AppID = "c0849608-c8b9-4e86-b37d-fce972a0a7f6";
         public static readonly AuthType AuthType = AuthType.DeviceCode;
         public static readonly List<string> AzureMfaPermissions = new List<string>() { "Directory.Read.All", "UserAuthenticationMethod.Read.All" };
+        public static readonly Uri DeviceLoginUrl = new Uri("https://microsoft.com/devicelogin");
         public static readonly ReportType ReportType = ReportType.CLI;
         public static readonly ReportPrintLevel PrintLevel = ReportPrintLevel.Brief;
     }
