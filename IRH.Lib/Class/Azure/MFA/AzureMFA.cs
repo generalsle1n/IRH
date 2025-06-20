b33f040a7e31917d9b616e30f67080b88d106cf9
Niels Schuler <niels.schuler@web.de>
2025-06-20 21:06:20 +0200

    Enhanced `AzureMFA` to process authentication methods with property extraction by adding `GetPropertiesFromAuthenticationMethod` helper.

--- IRH.Lib/Class/Azure/MFA/AzureMFA.cs
+++ IRH.Lib/Class/Azure/MFA/AzureMFA.cs
@@ -38,7 +38,14 @@ namespace IRH.Lib.Class.Azure.MFA
                     AllMFACount = AuthMethods.Value.Count
                 };
 
-                SingleUserResult.MFA.AddRange(AuthMethods.Value);
+                foreach (AuthenticationMethod SingleAuthMethod in AuthMethods.Value)
+                {
+                    SingleUserResult.MFA.Add(new AzureAuthenticationMethod()
+                    {
+                        Method = SingleAuthMethod,
+                        MethodProperties = await GetPropertiesFromAutheticationMethod(SingleAuthMethod)
+                    });
+                }
 
                 Result.Add(SingleUserResult);
                 _logger.Information($"Process MFA {Count} from {AllUsers.Value.Count}");
@@ -49,5 +56,33 @@ namespace IRH.Lib.Class.Azure.MFA
             return Result;
         }
 
+        public async Task<List<AzureAuthenticationMethodProperty>> GetPropertiesFromAutheticationMethod(AuthenticationMethod Method)
+        {
+            List<AzureAuthenticationMethodProperty> Result = new List<AzureAuthenticationMethodProperty>();
+            
+            PropertyInfo[] AllProperties = Method.GetType().GetProperties();
+            IEnumerable<PropertyInfo> AllStringVal = AllProperties.Where(prop => prop.PropertyType.Name.Equals("String"));
+
+            foreach (PropertyInfo Property in AllProperties)
+            {
+                AzureAuthenticationMethodProperty SingleProperty = new AzureAuthenticationMethodProperty()
+                {
+                    PropertyName = Property.Name,
+                    PropertyValue = Property.GetValue(Method),
+                    IsStringProperty = false,
+                    PropertyTypeName = Property.PropertyType.FullName,
+                };
+
+                if (Property.PropertyType.Name.Equals("String"))
+                {
+                    SingleProperty.IsStringProperty = true;
+                }
+                
+                Result.Add(SingleProperty);
+            }
+            
+            return Result;
+        }
+        
     }
 }
