91b9582605fea93e6be79854ff42fefdbe881f43
Niels Schuler <niels.schuler@web.de>
2025-06-20 21:10:37 +0200

    Updated `UserMFA` to replace `AuthenticationMethod` with `AzureAuthenticationMethod` for MFA property and added `IRH.Lib.Model.Azure.Auth` namespace.

--- IRH.Lib/Model/Azure/Result/UserMFA.cs
+++ IRH.Lib/Model/Azure/Result/UserMFA.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Graph.Models;
+﻿using IRH.Lib.Model.Azure.Auth;
+using Microsoft.Graph.Models;
 
 namespace IRH.Lib.Model.Azure.Result
 {
     public class UserMFA
     {
         public User User { get; set; }
-        public List<AuthenticationMethod> MFA { get; set; }
+        public List<AzureAuthenticationMethod> MFA { get; set; }
         public int AllMFACount {  get; set; }
     }
 }
