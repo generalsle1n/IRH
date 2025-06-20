92c4f06e93b9c3708b8df2752a2427e870f95dd5
Niels Schuler <niels.schuler@web.de>
2025-06-20 21:05:56 +0200

    Added `AzureAuthenticationMethod` and `AzureAuthenticationMethodProperty` models to support authentication method configurations in Azure.

---
+++ IRH.Lib/Model/Azure/Auth/AzureAuthenticationMethod.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Graph.Models;
+
+namespace IRH.Lib.Model.Azure.Auth;
+
+public class AzureAuthenticationMethod
+{
+    public List<AzureAuthenticationMethodProperty> MethodProperties { get; set; }
+    public AuthenticationMethod Method { get; set; }
+}
