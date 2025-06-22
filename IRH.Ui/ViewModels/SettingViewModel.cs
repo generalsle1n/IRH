63df6ba91b7d5ca7047d1cb3672ce31810a64af3
Niels Schuler <niels.schuler@web.de>
2025-06-22 11:31:39 +0200

    Refactored `SettingViewModel` to enhance `SelectedAppTheme` functionality by adding theme switching logic and synchronizing with `Preferences`. Removed redundant `OnSelectedAppThemeChanged`.

--- IRH.Ui/ViewModels/SettingViewModel.cs
+++ IRH.Ui/ViewModels/SettingViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.SimplePreferences;
+using Avalonia.Styling;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using IRH.Lib;
@@ -15,8 +17,44 @@ namespace IRH.Ui.ViewModels;
 
 internal partial class SettingViewModel : ViewModelBase
 {
-    [ObservableProperty]
     private AppTheme _selectedAppTheme = Preferences.Get<AppTheme>(Strings.Setting_Name_AppTheme, AppTheme.System);
+    public AppTheme SelectedAppTheme
+    {
+        get
+        {
+            switch (_selectedAppTheme)
+            {
+                case AppTheme.Light:
+                    Application.Current!.RequestedThemeVariant = ThemeVariant.Light;
+                    break;
+                case AppTheme.Dark:
+                    Application.Current!.RequestedThemeVariant = ThemeVariant.Dark;
+                    break;
+                case AppTheme.System:
+                    Application.Current!.RequestedThemeVariant = ThemeVariant.Default;
+                    break;
+            }
+            
+            return _selectedAppTheme;
+        }
+        set
+        {
+            switch (value)
+            {
+                case AppTheme.Light:
+                    Application.Current!.RequestedThemeVariant = ThemeVariant.Light;
+                    break;
+                case AppTheme.Dark:
+                    Application.Current!.RequestedThemeVariant = ThemeVariant.Dark;
+                    break;
+                case AppTheme.System:
+                    Application.Current!.RequestedThemeVariant = ThemeVariant.Default;
+                    break;
+            }
+            Preferences.Set<AppTheme>(Strings.Setting_Name_AppTheme, value);
+            SetProperty(ref _selectedAppTheme, value);
+        }
+    }
     [ObservableProperty]
     private AuthType _selectedAuthType = Preferences.Get<AuthType>(Strings.Setting_Name_AuthType, DefaultValue.AuthType);
     [ObservableProperty]
@@ -27,7 +65,6 @@ internal partial class SettingViewModel : ViewModelBase
     private bool _tenantIDEditEnabled = Preferences.Get<bool>(Strings.Setting_Name_TenantIDEditEnabled, false);
     [ObservableProperty]
     private bool _appIDEditEnabled = Preferences.Get<bool>(Strings.Setting_Name_AppIDEditEnabled, false);
-    
     internal List<AppTheme> AllAppThemes { get; } = Enum.GetValues<AppTheme>().Cast<AppTheme>().ToList();
     public List<AuthType> AllAuthTypes { get; } = Enum.GetValues<AuthType>().Cast<AuthType>().ToList();
 
@@ -48,12 +85,6 @@ internal partial class SettingViewModel : ViewModelBase
     private async Task ResetAllSettings(CancellationToken token)
     {
         await Preferences.ClearAsync(cancellationToken: token);
-    }
-    
-    partial void OnSelectedAppThemeChanged(AppTheme value)
-    {
-        Preferences.Set<AppTheme>(Resources.Strings.Setting_Name_AppTheme, value);
-        SelectedAppTheme = value;
     }
 
     partial void OnSelectedAuthTypeChanged(AuthType value)
