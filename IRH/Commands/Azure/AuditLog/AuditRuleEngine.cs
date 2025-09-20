4ffc1b73baf450b43d9c4a3e5be06112211d3825
Niels Schuler <niels.schuler@web.de>
2025-09-20 21:08:12 +0200

    Refactor Audit-Log-Engine und erweitere Optionen
    Die Klasse `AuditRuleEngine` wurde neu strukturiert, um die Lesbarkeit und Wartbarkeit zu verbessern, während die bestehende Funktionalität erhalten blieb. Methoden wie `ProcessAudit` und `IsMatchingRecordInfoLevel` wurden überarbeitet, um konsistenter zu sein.
    
    In `AzureAuditLog` wurden neue Optionen wie `StartDate` und `EndDate` hinzugefügt, um die Flexibilität bei der Untersuchung von Audit-Logs zu erhöhen. Die Befehlsstruktur wurde angepasst, um diese neuen Parameter zu unterstützen.
    
    Die Klasse `LoginAuditCommand` wurde vollständig überarbeitet. Neue Optionen wie `Scopes`, `Activities` und `WaitTime` wurden definiert, und die Unterstützung für bestehende Abfragen (`ExistingQuery`) wurde verbessert. Die Berichterstellungslogik unterstützt nun CLI- und JSON-Ausgabeformate.
    
    Diese Änderungen verbessern die Benutzerfreundlichkeit, Wartbarkeit und Funktionalität des Codes erheblich.

--- IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
+++ IRH/Commands/Azure/AuditLog/AuditRuleEngine.cs
@@ -1,169 +1,169 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
-//using System.Text;
-//using System.Text.RegularExpressions;
-//using System.Threading.Tasks;
-//using IRH.Commands.Azure.AuditLog.Model;
-//using IRH.Commands.Azure.Helper;
-//using Microsoft.Graph.Beta.Models.Security;
-//using Serilog.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using IRH.Commands.Azure.AuditLog.Model;
+using IRH.Commands.Azure.Helper;
+using Microsoft.Graph.Beta.Models.Security;
+using Serilog.Core;
 
-//namespace IRH.Commands.Azure.AuditLog
-//{
-//    internal class AuditRuleEngine
-//    {
-//        private List<AuditProcessorRule> _rules;
-//        private readonly Logger _logger;
-//        private const string _ruleSeperator = ":";
+namespace IRH.Commands.Azure.AuditLog
+{
+    internal class AuditRuleEngine
+    {
+        private List<AuditProcessorRule> _rules;
+        private readonly Logger _logger;
+        private const string _ruleSeperator = ":";
 
-//        internal AuditRuleEngine(string[] RuleText, Logger Logger)
-//        {
-//            _logger = Logger;
+        internal AuditRuleEngine(string[] RuleText, Logger Logger)
+        {
+            _logger = Logger;
 
-//            _rules = new List<AuditProcessorRule>();
+            _rules = new List<AuditProcessorRule>();
 
-//            foreach (string SingleRule in RuleText)
-//            {
-//                string[] Splitted = SingleRule.Split(_ruleSeperator);
+            foreach (string SingleRule in RuleText)
+            {
+                string[] Splitted = SingleRule.Split(_ruleSeperator);
 
-//                _rules.Add(new AuditProcessorRule()
-//                {
-//                    RawRule = new AuditRawRule()
-//                    {
-//                        ParamterNameFilter = Splitted[0],
-//                        ParamterValueFilter = Splitted[1]
-//                    },
-//                    RegexRule = new AuditRegexRule()
-//                    {
-//                        ParamterNameFilter = CreateRegex(Splitted[0]),
-//                        ParamterValueFilter = CreateRegex(Splitted[1])
-//                    }
-//                });
+                _rules.Add(new AuditProcessorRule()
+                {
+                    RawRule = new AuditRawRule()
+                    {
+                        ParamterNameFilter = Splitted[0],
+                        ParamterValueFilter = Splitted[1]
+                    },
+                    RegexRule = new AuditRegexRule()
+                    {
+                        ParamterNameFilter = CreateRegex(Splitted[0]),
+                        ParamterValueFilter = CreateRegex(Splitted[1])
+                    }
+                });
 
-//                _logger.Information($"Loaded {SingleRule} Rule into Engine");
-//            }
+                _logger.Information($"Loaded {SingleRule} Rule into Engine");
+            }
 
-//            _logger.Information($"{_rules.Count} Rules loaded");
-//        }
+            _logger.Information($"{_rules.Count} Rules loaded");
+        }
 
-//        internal async Task<bool> ProcessAudit(AuditLogRecord Record)
-//        {
-//            List<bool> AllResult = new List<bool>();
+        internal async Task<bool> ProcessAudit(AuditLogRecord Record)
+        {
+            List<bool> AllResult = new List<bool>();
 
-//            foreach (AuditProcessorRule SingleRule in _rules)
-//            {
-//                bool SearchResult = false;
+            foreach (AuditProcessorRule SingleRule in _rules)
+            {
+                bool SearchResult = false;
 
-//                SearchResult = await IsMatchingRecordInfoLevel(Record, SingleRule);
+                SearchResult = await IsMatchingRecordInfoLevel(Record, SingleRule);
 
-//                if(!SearchResult)
-//                {
-//                    SearchResult = await IsMatchingRecordAuditLevel(Record, SingleRule);
+                if (!SearchResult)
+                {
+                    SearchResult = await IsMatchingRecordAuditLevel(Record, SingleRule);
 
-//                    if (!SearchResult)
-//                    {
-//                        SearchResult = await IsMatchingRecordExpandedLevel(Record, SingleRule);
-//                    }
-//                }
+                    if (!SearchResult)
+                    {
+                        SearchResult = await IsMatchingRecordExpandedLevel(Record, SingleRule);
+                    }
+                }
 
-//                AllResult.Add(SearchResult);
+                AllResult.Add(SearchResult);
 
-//            }
+            }
 
-//            bool Result = AllResult.All(filter => filter == true);
+            bool Result = AllResult.All(filter => filter == true);
 
-//            return Result;
-//        }
+            return Result;
+        }
 
-//        internal async Task<bool> IsMatchingRecordInfoLevel(AuditLogRecord Record, AuditProcessorRule Rule)
-//        {
-//            bool SingleResult = false;
+        internal async Task<bool> IsMatchingRecordInfoLevel(AuditLogRecord Record, AuditProcessorRule Rule)
+        {
+            bool SingleResult = false;
 
-//            PropertyInfo[] AllProperties = Record.GetType().GetProperties();
+            PropertyInfo[] AllProperties = Record.GetType().GetProperties();
 
-//            IEnumerable<PropertyInfo> Search = AllProperties.Where(filter => filter.Name.Equals(Rule.RawRule.ParamterNameFilter));
+            IEnumerable<PropertyInfo> Search = AllProperties.Where(filter => filter.Name.Equals(Rule.RawRule.ParamterNameFilter));
 
-//            _logger.Verbose($"Evaluted {AllProperties.Length} Properties for Object {Record.Id}");
+            _logger.Verbose($"Evaluted {AllProperties.Length} Properties for Object {Record.Id}");
 
-//            if (Search.Count() > 0)
-//            {
-//                PropertyInfo FilterdProperty = Search.First();
-//                string PropertyValue = FilterdProperty.GetValue(Record) as string;
+            if (Search.Count() > 0)
+            {
+                PropertyInfo FilterdProperty = Search.First();
+                string PropertyValue = FilterdProperty.GetValue(Record) as string;
 
-//                _logger.Verbose($"Searching {FilterdProperty.Name} in {Record.Id}");
+                _logger.Verbose($"Searching {FilterdProperty.Name} in {Record.Id}");
 
-//                if (await MatchStringToRegex(PropertyValue, Rule.RegexRule.ParamterValueFilter))
-//                {
-//                    SingleResult = true;
-//                }
+                if (await MatchStringToRegex(PropertyValue, Rule.RegexRule.ParamterValueFilter))
+                {
+                    SingleResult = true;
+                }
 
-//                _logger.Verbose($"Match for {FilterdProperty.Name}: {SingleResult}");
-//            }
+                _logger.Verbose($"Match for {FilterdProperty.Name}: {SingleResult}");
+            }
 
-//            return SingleResult;
-//        }
+            return SingleResult;
+        }
 
-//        internal async Task<bool> IsMatchingRecordAuditLevel(AuditLogRecord Record, AuditProcessorRule Rule)
-//        {
-//            bool SingleResult = false;
+        internal async Task<bool> IsMatchingRecordAuditLevel(AuditLogRecord Record, AuditProcessorRule Rule)
+        {
+            bool SingleResult = false;
 
-//            IAsyncEnumerable<KeyValuePair<string, object>> Search = Record.AuditData.AdditionalData.ToAsyncEnumerable().WhereAwait(async filter =>
-//            {
-//                return (filter.Key.Equals(Rule.RawRule.ParamterNameFilter) && await MatchStringToRegex(filter.Value.ToString(), Rule.RegexRule.ParamterValueFilter));
-//            });
+            IAsyncEnumerable<KeyValuePair<string, object>> Search = Record.AuditData.AdditionalData.ToAsyncEnumerable().WhereAwait(async filter =>
+            {
+                return (filter.Key.Equals(Rule.RawRule.ParamterNameFilter) && await MatchStringToRegex(filter.Value.ToString(), Rule.RegexRule.ParamterValueFilter));
+            });
 
-//            _logger.Verbose($"Evaluted {Record.AuditData.AdditionalData.Count} Properties for Object {Record.Id}");
+            _logger.Verbose($"Evaluted {Record.AuditData.AdditionalData.Count} Properties for Object {Record.Id}");
 
-//            if (await Search.CountAsync() > 0)
-//            {
-//                SingleResult = true;
-//                _logger.Verbose($"Match for {(await Search.FirstAsync()).Key}: {SingleResult}");
-//            }
+            if (await Search.CountAsync() > 0)
+            {
+                SingleResult = true;
+                _logger.Verbose($"Match for {(await Search.FirstAsync()).Key}: {SingleResult}");
+            }
 
-//            return SingleResult;
-//        }
+            return SingleResult;
+        }
 
-//        internal async Task<bool> IsMatchingRecordExpandedLevel(AuditLogRecord Record, AuditProcessorRule Rule)
-//        {
-//            bool SingleResult = false;
+        internal async Task<bool> IsMatchingRecordExpandedLevel(AuditLogRecord Record, AuditProcessorRule Rule)
+        {
+            bool SingleResult = false;
 
-//            _logger.Verbose($"Try to expand object{Record.Id} for further search");
+            _logger.Verbose($"Try to expand object{Record.Id} for further search");
 
-//            List<KeyValuePair<string, string>> Expanded = await UnTypedExtractor.ExtractUntypedDataFromAuditLogRecord(Record);
+            List<KeyValuePair<string, string>> Expanded = await UnTypedExtractor.ExtractUntypedDataFromAuditLogRecord(Record);
 
-//            IAsyncEnumerable<KeyValuePair<string, string>> SearchForExpand = Expanded.ToAsyncEnumerable().WhereAwait(async filter =>
-//            {
-//                return filter.Key.Equals(Rule.RawRule.ParamterNameFilter) && await MatchStringToRegex(filter.Value as string, Rule.RegexRule.ParamterValueFilter);
-//            });
+            IAsyncEnumerable<KeyValuePair<string, string>> SearchForExpand = Expanded.ToAsyncEnumerable().WhereAwait(async filter =>
+            {
+                return filter.Key.Equals(Rule.RawRule.ParamterNameFilter) && await MatchStringToRegex(filter.Value as string, Rule.RegexRule.ParamterValueFilter);
+            });
 
-//            if (await SearchForExpand.CountAsync() > 0)
-//            {
-//                SingleResult = true;
-//                _logger.Verbose($"Match for Expanded {Rule.RawRule.ParamterNameFilter}: {SingleResult}");
-//            }
+            if (await SearchForExpand.CountAsync() > 0)
+            {
+                SingleResult = true;
+                _logger.Verbose($"Match for Expanded {Rule.RawRule.ParamterNameFilter}: {SingleResult}");
+            }
 
-//            return SingleResult;
-//        }
+            return SingleResult;
+        }
 
-//        internal Regex CreateRegex(string Input)
-//        {
-//            string CurrentPattern = $"^{Input.Replace("*", ".*")}$";
-//            return new Regex(CurrentPattern, RegexOptions.IgnoreCase);
-//        }
+        internal Regex CreateRegex(string Input)
+        {
+            string CurrentPattern = $"^{Input.Replace("*", ".*")}$";
+            return new Regex(CurrentPattern, RegexOptions.IgnoreCase);
+        }
 
-//        internal async Task<bool> MatchStringToRegex(string Value, Regex Regex)
-//        {
-//            bool Matched = false;
+        internal async Task<bool> MatchStringToRegex(string Value, Regex Regex)
+        {
+            bool Matched = false;
 
-//            Match Match = Regex.Match(Value);
-//            if (Match.Success)
-//            {
-//                Matched = true;
-//            }
+            Match Match = Regex.Match(Value);
+            if (Match.Success)
+            {
+                Matched = true;
+            }
 
-//            return Matched;
-//        }
-//    }
-//}
+            return Matched;
+        }
+    }
+}
--- IRH/Commands/Azure/AuditLog/AzureAuditLog.cs
+++ IRH/Commands/Azure/AuditLog/AzureAuditLog.cs
@@ -1,55 +1,87 @@
-﻿//using IRH.Commands.Azure.AuditLog.Exchange;
-//using IRH.Commands.Azure.AuditLog.Login;
-//using Serilog.Core;
-//using System.CommandLine;
+﻿using IRH.Commands.Azure.AuditLog.Exchange;
+using IRH.Commands.Azure.AuditLog.Login;
+using Serilog.Core;
+using System.CommandLine;
 
-//namespace IRH.Commands.Azure.AuditLog
-//{
-//    internal class AzureAuditLog
-//    {
-//        private const string _commandName = "-Audit";
-//        private const string _commandDescription = "Operate with the Audit System from Microsoft";
+namespace IRH.Commands.Azure.AuditLog
+{
+    internal class AzureAuditLog
+    {
+        private const string _commandName = "-Audit";
+        private const string _commandDescription = "Operate with the Audit System from Microsoft";
 
-//        private const string _filterOnParameter = "-FP";
-//        private const string _filterOnParameterDescription = "Filter on Parameternames (Displayfilter), Wildcards are supported (This Setting works only on Printlevel Info and above) its also possible to enter multiple values seperated by whitespace";
-//        private const string _filterOnParameterAlias = "--FilterParameter";
+        internal const string _filterOnParameter = "-FP";
+        private const string _filterOnParameterDescription = "Filter on Parameternames (Displayfilter), Wildcards are supported (This Setting works only on Printlevel Info and above) its also possible to enter multiple values seperated by whitespace";
+        private const string _filterOnParameterAlias = "--FilterParameter";
 
-//        private const string _filterOnParameterValue = "-FV";
-//        private const string _filterOnParameterValueDescription = "Filter on Paramtervalue (Datafilter): Syntax --> ParamterName:ParameterValue (Example: Id:241af6fe-955d-4884-b27d-08dc93695d85), if you specify multiple serpated by whitespace it have an AND Operator, there is an Wildcard Support for the parametervalue";
-//        private const string _filterOnParameterValueAlias = "--FilterValue";
+        internal const string _filterOnParameterValue = "-FV";
+        private const string _filterOnParameterValueDescription = "Filter on Paramtervalue (Datafilter): Syntax --> ParamterName:ParameterValue (Example: Id:241af6fe-955d-4884-b27d-08dc93695d85), if you specify multiple serpated by whitespace it have an AND Operator, there is an Wildcard Support for the parametervalue";
+        private const string _filterOnParameterValueAlias = "--FilterValue";
 
-//        private readonly Logger _logger;
+        internal const string _startDate = "-S";
+        private const string _startDateDescription = "Enter the Start of the Investigation (Just in Format DD.MM.YYYY)";
+        private const string _startDateAlias = "--Start";
+        private DateTime _startDateDefaultValue = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
 
-//        internal AzureAuditLog(Logger Logger)
-//        {
-//            _logger = Logger;
-//        }
+        internal const string _endDate = "-E";
+        private const string _endDateDescription = "Enter the End of the Investigation (Just in Format DD.MM.YYYY)";
+        private const string _endDateAlias = "--End";
+        private DateTime _endDateDefaultValue = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddDays(1).AddTicks(-1);
 
-//        internal Command CreateCommand(RootCommand RootCommand)
-//        {
-//            Command Command = new Command(name: _commandName, description: _commandDescription);
+        private readonly Logger _logger;
 
-//            Option<string[]> FilterOnParameter = new Option<string[]>(name: _filterOnParameter, description: _filterOnParameterDescription);
-//            Option<string[]> FilterOnParameterValue = new Option<string[]>(name: _filterOnParameterValue, description: _filterOnParameterValueDescription);
-            
-//            FilterOnParameter.AddAlias(_filterOnParameterAlias);
-//            FilterOnParameterValue.AddAlias(_filterOnParameterValueAlias);
+        internal AzureAuditLog(Logger Logger)
+        {
+            _logger = Logger;
+        }
 
-//            FilterOnParameter.AllowMultipleArgumentsPerToken = true;
-//            FilterOnParameterValue.AllowMultipleArgumentsPerToken = true;
+        internal Command CreateCommand(RootCommand RootCommand)
+        {
+            Command Command = new Command(name: _commandName, description: _commandDescription);
 
-//            Command.AddGlobalOption(FilterOnParameter);
-//            Command.AddGlobalOption(FilterOnParameterValue);
+            Option<string[]> FilterOnParameter = new Option<string[]>(name: _filterOnParameter, aliases: _filterOnParameterAlias)
+            {
+                Description = _filterOnParameterDescription,
+                AllowMultipleArgumentsPerToken = true,
+                Recursive = true
+            };
 
-//            ExchangeAudit ExchangeAuditCommand = new ExchangeAudit(_logger);
-//            Command ExchangeCommand = ExchangeAuditCommand.CreateCommand(RootCommand);
-            
-//            LoginAuditCommand LoginAuditCommand = new LoginAuditCommand(_logger);
-//            Command LoginCommand = LoginAuditCommand.CreateCommand(RootCommand);
+            Option<string[]> FilterOnParameterValue = new Option<string[]>(name: _filterOnParameterValue, aliases: _filterOnParameterValueAlias)
+            {
+                Description = _filterOnParameterValueDescription,
+                AllowMultipleArgumentsPerToken = true,
+                Recursive = true
+            };
 
-//            Command.AddCommand(ExchangeCommand);
-//            Command.AddCommand(LoginCommand);
-//            return Command;
-//        }
-//    }
-//}
+            Option<DateTime> StartDate = new Option<DateTime>(name: _startDate, aliases: _startDateAlias)
+            {
+                Description = _startDateDescription,
+                DefaultValueFactory = (result) => _startDateDefaultValue,
+                Recursive = true
+            };
+
+            Option<DateTime> EndDate = new Option<DateTime>(name: _endDate, aliases: _endDateAlias)
+            {
+                Description = _endDateDescription,
+                DefaultValueFactory = (result) => _endDateDefaultValue,
+                Recursive = true
+            };
+
+            Command.Options.Add(FilterOnParameter);
+            Command.Options.Add(FilterOnParameterValue);
+            Command.Options.Add(StartDate);
+            Command.Options.Add(EndDate);
+
+            ExchangeAudit ExchangeAuditCommand = new ExchangeAudit(_logger);
+            Command ExchangeCommand = ExchangeAuditCommand.CreateCommand(RootCommand);
+
+            LoginAuditCommand LoginAuditCommand = new LoginAuditCommand(_logger);
+            Command LoginCommand = LoginAuditCommand.CreateCommand(RootCommand);
+
+            Command.Add(ExchangeCommand);
+            Command.Add(LoginCommand);
+
+            return Command;
+        }
+    }
+}
