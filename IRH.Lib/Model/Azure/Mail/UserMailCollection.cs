1e9d270eb108c3f428025ab55113d374903460fe
Niels Schuler <niels.schuler@web.de>
2025-09-21 18:57:40 +0200

    Neue Klasse UserMailCollection hinzugefügt
    Eine neue Klasse `UserMailCollection` wurde im Namespace
    `IRH.Lib.Model.Azure.Mail` erstellt. Diese Klasse enthält
    zwei Eigenschaften: `User` (vom Typ `User`) und `Mail`
    (eine Liste vom Typ `List<Message>`). Zusätzlich wurden
    die erforderlichen `using`-Anweisungen hinzugefügt, um
    die benötigten Namespaces einzubinden.

---
+++ IRH.Lib/Model/Azure/Mail/UserMailCollection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Graph.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Lib.Model.Azure.Mail
+{
+    public class UserMailCollection
+    {
+        public User User { get; set; }
+        public List<Message> Mail { get; set; }
+    }
+}
