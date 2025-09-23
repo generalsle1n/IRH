f441934a19d7075aa559fab5463a30717de53c1d
Niels Schuler <niels.schuler@web.de>
2025-09-23 08:14:58 +0200

    Integrated AzureGroupView, AzureDateView, AzureScopeView, and AzureActionBarView into AzureMailView with updated bindings and corresponding ViewModel properties.

--- IRH.Ui/ViewModels/AzureMailViewModel.cs
+++ IRH.Ui/ViewModels/AzureMailViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace IRH.Ui.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using IRH.Lib;
+using IRH.Ui.Lib;
+using IRH.Ui.Models.Azure;
+using IRH.Ui.ViewModels.Template;
+
+namespace IRH.Ui.ViewModels;
 
 public partial class AzureMailViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private AzureActionBarViewModel _azureActionBarViewModel = new AzureActionBarViewModel();
     
+    [ObservableProperty]
+    private AzureScopeViewModel _azureScopeViewModel = new AzureScopeViewModel()
+    {
+        AllScopes = UIHelper.CreateObservableItemControlTemplateFromList(DefaultValue.AzureMailCleanupPermissions)
+    };
+    
+    [ObservableProperty]
+    private AzureGroupViewModel _azureGroupViewModel = new AzureGroupViewModel();
+    [ObservableProperty]
+    private AzureDateViewModel _azureDateViewModel = new AzureDateViewModel();
 }
