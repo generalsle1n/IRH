42c20fca3cc600f4325fc110f31af43b574f2f89
Niels Schuler <niels.schuler@web.de>
2025-09-23 08:14:11 +0200

    Added new AzureScopeView and corresponding ViewModel

---
+++ IRH.Ui/ViewModels/Template/AzureScopeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.Input;
+using IRH.Lib;
+using IRH.Ui.Models.Azure;
+
+namespace IRH.Ui.ViewModels.Template;
+
+public partial class AzureScopeViewModel : ViewModelBase
+{
+    internal ObservableCollection<AzureItemControlTemplate> AllScopes { get; set; } = new ObservableCollection<AzureItemControlTemplate>();
+    
+    [RelayCommand]
+    private async Task AddNewScope()
+    {
+        AllScopes.Add(new AzureItemControlTemplate(null));
+    }
+
+    [RelayCommand]
+    private async Task DeleteScope(object Sender)
+    {
+        Button SingleButton = Sender as Button;
+        AzureItemControlTemplate Item = SingleButton.DataContext as AzureItemControlTemplate;
+        AllScopes.Remove(Item);
+    }
+}
