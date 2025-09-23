af9f744fa21d94893f67abe964d487d48d17560b
Niels Schuler <niels.schuler@web.de>
2025-09-23 08:14:00 +0200

    Added new AzureDateView and corresponding ViewModel

---
+++ IRH.Ui/ViewModels/Template/AzureDateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace IRH.Ui.ViewModels.Template;
+
+public partial class AzureDateViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    DateTime _selectedStartDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+    [ObservableProperty]
+    DateTime _selectedEndDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+    [ObservableProperty]
+    TimeSpan _selectedStartTime = new TimeSpan(0, 0, 0);
+    [ObservableProperty]
+    TimeSpan _selectedEndTime = new TimeSpan(23, 59, 59);
+}
