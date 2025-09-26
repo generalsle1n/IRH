b633495766baa81d9f2e2c98eb44f5d633449aca
Niels Schuler <niels.schuler@web.de>
2025-09-26 09:17:02 +0200

    Add OpenMailAsync method and AzureActionViewModel to AzureMailViewModel

--- IRH.Ui/ViewModels/AzureMailViewModel.cs
+++ IRH.Ui/ViewModels/AzureMailViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
 using IRH.Lib;
 using IRH.Lib.Model.Azure.Mail;
 using IRH.Ui.Lib;
+using IRH.Ui.Models.Azure;
 using IRH.Ui.ViewModels.Template;
 
 namespace IRH.Ui.ViewModels;
@@ -36,6 +41,35 @@ public partial class AzureMailViewModel : ViewModelBase, IRecipient<List<UserMai
     {
         AllScopes = UiHelper.CreateObservableItemControlTemplateFromList(DefaultValue.AzureMailCleanupPermissions)
     };
+    
+    [ObservableProperty]
+    private AzureActionViewModel _azureActionViewModel = new AzureActionViewModel()
+    {
+        AllActions = new ObservableCollection<string>()
+        {
+            Resources.Strings.Azure_Mail_Action_Preview_Text,
+            Resources.Strings.Azure_Mail_Action_Delete_Text
+        }
+    };
+    
+    private UiHelper _uiHelper = new UiHelper();
+    
+    [RelayCommand]
+    private async Task OpenMailAsync(MailStatus mailStatus, CancellationToken token)
+    {
+        string TempFile = Path.GetTempFileName();
+        string HtmlTempFile = Path.ChangeExtension(TempFile, ".html");
+        
+        File.Move(TempFile, HtmlTempFile);
+
+        using (FileStream Stream = new FileStream(HtmlTempFile, FileMode.Open, FileAccess.ReadWrite))
+        using (StreamWriter Writer = new StreamWriter(Stream))    
+        {
+            await Writer.WriteAsync(mailStatus.Mail.Body.Content);
+        }
+
+        await _uiHelper.OpenUrlInBrowserAsync(new Uri(HtmlTempFile));
+    }
     
     public void Receive(List<UserMailCollection> message)
     {
