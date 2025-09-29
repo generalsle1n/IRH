938dbec1056a9e9e17cc5b79bfd432527604b978
Niels Schuler <niels.schuler@web.de>
2025-09-29 18:02:12 +0200

    Add CommunityToolkit.Mvvm for observable property support in UserMailCollection

--- IRH.Lib/Model/Azure/Mail/UserMailCollection.cs
+++ IRH.Lib/Model/Azure/Mail/UserMailCollection.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace IRH.Lib.Model.Azure.Mail
 {
-    public class UserMailCollection
+    public partial class UserMailCollection : ObservableObject
     {
         public User User { get; set; }
         public MailStatus MailStatus { get; set; }
-        public bool Deleted { get; set; }
         
+        [ObservableProperty] 
+        private bool _deleted = false;
+
     }
 }
