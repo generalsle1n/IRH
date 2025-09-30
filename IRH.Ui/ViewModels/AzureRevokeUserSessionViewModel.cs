1935024478a3a3cb3bc987e3a9866cc9090930c2
Niels Schuler <niels.schuler@web.de>
2025-09-30 16:36:30 +0200

    Refactored `AzureRevokeUserSessionViewModel` to inherit from `AzureViewModelBase`, consolidating logic and replacing redundant collections and methods with streamlined counterparts.

--- IRH.Ui/ViewModels/AzureRevokeUserSessionViewModel.cs
+++ IRH.Ui/ViewModels/AzureRevokeUserSessionViewModel.cs
@@ -1,194 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Platform.Storage;
-using Avalonia.SimplePreferences;
-using Azure.Identity;
+﻿using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using IRH.Lib;
-using IRH.Lib.Class.Azure.Auth;
 using IRH.Lib.Class.Azure.Generel;
 using IRH.Lib.Class.Azure.Session;
-using IRH.Lib.Model.Azure.Auth;
-using IRH.Lib.Model.Azure.Reporting;
-using IRH.Lib.Model.Azure.Result;
 using IRH.Lib.Model.Azure.Session;
 using IRH.Ui.Lib;
-using IRH.Ui.Models.Azure;
-using IRH.Ui.Resources;
-using Microsoft.Graph;
+using IRH.Ui.Models.Message.Send;
+using IRH.Ui.ViewModels.Template;
 using Microsoft.Graph.Models;
 using Serilog;
-using Application = Avalonia.Application;
 
 namespace IRH.Ui.ViewModels;
 
-public partial class AzureRevokeUserSessionViewModel : ViewModelBase
+public partial class AzureRevokeUserSessionViewModel : AzureViewModelBase<UserSession, AzureRevokeUserSessionViewModel>
 {
+    [ObservableProperty]
+    private AzureGroupViewModel _azureGroupViewModel = new AzureGroupViewModel();
     
-    
-    private UiHelper _uiHelper = new UiHelper();
-    [ObservableProperty] 
-    private bool _openBrowserEnabled = false;
-    [ObservableProperty] 
-    private string _userCode;
-    [ObservableProperty] 
-    private bool _copyUserCodeEnabled = false;
-    [ObservableProperty] 
-    private ReportPrintLevel _selectedReportLevel = DefaultValue.PrintLevel;
-    [ObservableProperty] 
-    private bool _loadingRingEnabled = false;
-    [ObservableProperty] 
-    private bool _exportEnabled = false;
-    
-    public ObservableCollection<UserSession> AllUserSessionData { get; } = new ObservableCollection<UserSession>();
-    public ObservableCollection<AzureItemControlTemplate> AllGroupFilter { get; } = new ObservableCollection<AzureItemControlTemplate>()
+    [ObservableProperty]
+    private AzureScopeViewModel _azureScopeViewModel = new AzureScopeViewModel()
     {
-        new AzureItemControlTemplate(null, showDelete: false)
+        AllScopes = UiHelper.CreateObservableItemControlTemplateFromList(DefaultValue.AzureSessionRevokePermissions)
     };
-    public ObservableCollection<AzureItemControlTemplate> AllScopes { get; } = new ObservableCollection<AzureItemControlTemplate>(
-        DefaultValue.AzureSessionPermissions.Select((singleString, index) =>
-            new AzureItemControlTemplate(singleString, showDelete: index != 0))
-    );
-    
-    internal List<ReportPrintLevel> AllReportLevel { get; } = Enum.GetValues<ReportPrintLevel>().Cast<ReportPrintLevel>().ToList();
-    
-    [RelayCommand]
-    private async Task AddNewGroupFilter()
+
+    protected override async Task StartAzureProcessAsync(AzureGraphViewModelMessageGeneric<AzureRevokeUserSessionViewModel> message)
     {
-        AllGroupFilter.Add(new AzureItemControlTemplate(null));
-    }
-
-    [RelayCommand]
-    private async Task DeleteGroupFilter(object Sender)
-    {
-        Button SingleButton = Sender as Button;
-        AzureItemControlTemplate Item = SingleButton.DataContext as AzureItemControlTemplate;
-        AllGroupFilter.Remove(Item);
-    }
-    
-    [RelayCommand]
-    private async Task AddNewScope()
-    {
-        AllScopes.Add(new AzureItemControlTemplate(null));
-    }
-
-    [RelayCommand]
-    private async Task DeleteScope(object Sender)
-    {
-        Button SingleButton = Sender as Button;
-        AzureItemControlTemplate Item = SingleButton.DataContext as AzureItemControlTemplate;
-        AllScopes.Remove(Item);
-    }
-    
-    [RelayCommand]
-    private async Task SetUserCodeToClipboard()
-    {
-        await _uiHelper.SetTextToClipboard(UserCode);
-    }
-
-    [RelayCommand]
-    private async Task OpenBrowserAsync()
-    {
-        await _uiHelper.OpenUrlInBrowserAsync(DefaultValue.DeviceLoginUrl);
-    }
-    
-    [RelayCommand]
-    private async Task LoadDataFile(CancellationToken token)
-    {
-        IReadOnlyList<IStorageFile> OpenFile = await _uiHelper.GetIStorageFileListForOpenFile();
-
-        if (OpenFile.Any())
-        {
-            Uri SinglePath = OpenFile[0].Path;
-            using (FileStream Stream = new FileStream(SinglePath.AbsolutePath, FileMode.Open, FileAccess.Read))
-            {
-                List<UserSession> Result = await JsonSerializer.DeserializeAsync<List<UserSession>>(Stream, cancellationToken: token);
-                AllUserSessionData.Clear();
-                foreach (UserSession SingleUser in Result)
-                {
-                    AllUserSessionData.Add(SingleUser);
-                }
-            }
-        }
-    }
-    
-    [RelayCommand]
-    private async Task SaveDataToFile(CancellationToken token)
-    {
-        IStorageFile SaveFile = await _uiHelper.GetIStorageFileListForCreateFile();
-        
-        if (SaveFile is not null)
-        {
-            Uri SinglePath = SaveFile.Path;
-            using (FileStream Stream = new FileStream(SinglePath.AbsolutePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                await JsonSerializer.SerializeAsync<List<UserSession>>(Stream, AllUserSessionData.ToList(), cancellationToken: token);
-            }
-        }
-    }
-
-    [RelayCommand]
-    private async Task StartAzureGathering(CancellationToken token)
-    {
-        LoadingRingEnabled = true;
-
-        AuthType Flow = Preferences.Get<AuthType>(Strings.Setting_Name_AuthType, AuthType.DeviceCode);
-        GraphServiceClient Client = null;
-        AzureAuth AzureAuth = new AzureAuth(Log.Logger);
-
-        switch (Flow)
-        {
-            case AuthType.DeviceCode:
-                DeviceCodeCredentialOptions DeviceCodeCredentialOptions = AzureAuth.CreateDeviceCodeCredentialOptions(
-                    Preferences.Get<String>(Strings.Setting_Name_AppID, DefaultValue.AppId),
-                    Preferences.Get<String>(Strings.Setting_Name_TenantID, DefaultValue.TenantId),
-                    CreateCallBack: false);
-
-                DeviceCodeCredentialOptions.DeviceCodeCallback += (DeviceCode, sender) =>
-                {
-                    UserCode = DeviceCode.UserCode;
-                    CopyUserCodeEnabled = true;
-                    OpenBrowserEnabled = true;
-
-                    return Task.CompletedTask;
-                };
-
-                DeviceCodeCredential DeviceCodeCredential = AzureAuth.CreateDeviceCodeCredential(DeviceCodeCredentialOptions);
-
-                Client = await AzureAuth.GetClientAsync(
-                    Preferences.Get<String>(Strings.Setting_Name_AppID, DefaultValue.AppId),
-                    Preferences.Get<String>(Strings.Setting_Name_TenantID, DefaultValue.TenantId),
-                    _uiHelper.GetContentFromObservableCollection(AllScopes),
-                    Flow,
-                    CodeCredential: DeviceCodeCredential);
-                break;
-            case AuthType.Interactive:
-                Client = Client = await AzureAuth.GetClientAsync(
-                    Preferences.Get<String>(Strings.Setting_Name_AppID, DefaultValue.AppId),
-                    Preferences.Get<String>(Strings.Setting_Name_TenantID, DefaultValue.TenantId),
-                    _uiHelper.GetContentFromObservableCollection(AllScopes),
-                    Flow);
-                break;
-        }
-
-        string[] AllGroups = _uiHelper.GetContentFromObservableCollection(AllGroupFilter, removeEmpty: true);
+        string[] AllGroups = UiHelper.GetContentFromObservableCollection(AzureGroupViewModel.AllGroupFilter, removeEmpty: true);
 
         AzureUser AzureUser = new AzureUser(Log.Logger);
-        UserCollectionResponse AllUser = await AzureUser.GetUsersAsync(Client, AllGroups, token);
+        UserCollectionResponse Users = await AzureUser.GetUsersAsync(message.Client, AllGroups);
         
-        foreach (User SingleUser in AllUser.Value)
+        AllItems.Clear();
+        foreach (User SingleUser in Users.Value)
         {
-            AllUserSessionData.Add(new UserSession()
+            AllItems.Add(new UserSession()
             {
                 User = SingleUser,
                 ResetToken = false
@@ -197,9 +42,9 @@ public partial class AzureRevokeUserSessionViewModel : ViewModelBase
 
         AzureSession AzureSession = new AzureSession(Log.Logger);
 
-        foreach (UserSession SingleUserSession in AllUserSessionData)
+        foreach (UserSession SingleUserSession in AllItems)
         {
-            UserSession Result = await AzureSession.ResetSingleUserSessionAsync(Client, SingleUserSession);
+            UserSession Result = await AzureSession.ResetSingleUserSessionAsync(message.Client, SingleUserSession);
             SingleUserSession.ResetToken = Result.ResetToken;
             SingleUserSession.Response = Result.Response;
         }
