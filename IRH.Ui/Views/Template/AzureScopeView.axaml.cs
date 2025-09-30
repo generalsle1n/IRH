4d497dafa5b84db320859de54ca59efd171dca1d
Niels Schuler <niels.schuler@web.de>
2025-09-30 22:44:29 +0200

    Unregistered all messages in `AzureScopeView` `OnUnloaded` to prevent memory leaks.

--- IRH.Ui/Views/Template/AzureScopeView.axaml.cs
+++ IRH.Ui/Views/Template/AzureScopeView.axaml.cs
@@ -1,6 +1,6 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace IRH.Ui.Views.Template;
 
@@ -9,5 +9,11 @@ public partial class AzureScopeView : UserControl
     public AzureScopeView()
     {
         InitializeComponent();
+    }
+ 
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        WeakReferenceMessenger.Default.UnregisterAll(DataContext);
+        base.OnUnloaded(e);
     }
 }
