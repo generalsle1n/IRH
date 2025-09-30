60841ff9c86f178da3370617089f51857b0594af
Niels Schuler <niels.schuler@web.de>
2025-09-30 15:51:59 +0200

    Converted `UserSession` to a partial class with `ObservableObject` inheritance and replaced `

--- IRH.Lib/Model/Azure/Session/UserSession.cs
+++ IRH.Lib/Model/Azure/Session/UserSession.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Graph.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Graph.Models;
 using Microsoft.Graph.Users.Item.RevokeSignInSessions;
 
 namespace IRH.Lib.Model.Azure.Session
 {
-    public class UserSession
+    public partial class UserSession : ObservableObject
     {
         public User User { get; set; }
-        public bool ResetToken {  get; set; }
+
+        [ObservableProperty] 
+        private bool _resetToken;
         public RevokeSignInSessionsPostResponse Response { get; set; }
     }
 }
