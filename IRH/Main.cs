51efb74578b1d51a8738d64292bd909070bac7c2
Niels Schuler <niels.schuler@web.de>
2025-10-06 22:12:44 +0200

    Added `RemoteFunctions` command to integrate remote operation capabilities into the CLI, enabling execution of remote functions.

--- IRH/Main.cs
+++ IRH/Main.cs
@@ -5,6 +5,7 @@ using Serilog;
 using Serilog.Core;
 using System.CommandLine;
 using IRH.Commands.Azure;
+using IRH.Commands.Remote;
 using IRH.Remote;
 
 const string _commandDescription = "suite of some little helper tools within incident response when dealing with security breaches. These tools provide essential features for IT security professionals, making it easier to manage and respond to incidents effectively.";
@@ -17,15 +18,15 @@ Logger Logger = new LoggerConfiguration()
 RootCommand RootCommand = new RootCommand(_commandDescription);
 
 LDAPMonitor LM = new LDAPMonitor(Logger);
-//RemoteExecution RM = new RemoteExecution(Logger);
+RemoteFunctions RM = new RemoteFunctions(Logger);
 AzureFunctions AF = new AzureFunctions(Logger);
 
 Command LdapMonitor = LM.CreateCommand(RootCommand);
-//Command RemoteExecution = RM.CreateCommand(RootCommand);
+Command RemoteFunction = RM.CreateCommand(RootCommand);
 Command AzureFunctions = AF.CreateCommand(RootCommand);
 
 RootCommand.Subcommands.Add(LdapMonitor);
-//RootCommand.Subcommands.Add(RemoteExecution);
+RootCommand.Subcommands.Add(RemoteFunction);
 RootCommand.Subcommands.Add(AzureFunctions);
 
 ParseResult result = RootCommand.Parse(args);
