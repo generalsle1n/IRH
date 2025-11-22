202f22f9eca6f9eea0b94facd92532fb088ed290
Niels <niels.schuler@web.de>
2025-11-22 15:58:15 +0100

    Neue HTTP-Handler für Anfrage-Interzeption hinzugefügt
    Es wurden zwei neue Klassen hinzugefügt:
    - `VMWareInterceptingHttpMessageHandler` in `VMWareInterceptingHttpMessageHandler.cs`, die als `DelegatingHandler` fungiert und die Interzeption von HTTP-Anfragen und -Antworten ermöglicht. Sie unterstützt benutzerdefinierte Logik vor dem Senden (`OnSending`) und nach dem Empfang (`OnSent`) von HTTP-Nachrichten.
    - `VMwareHttpMessageHandlerBehavior` in `VMwareHttpMessageHandlerBehavior.cs`, die das Interface `IEndpointBehavior` implementiert und die Anpassung des HTTP-Handlers für WCF-Endpunkte erlaubt. Diese Klasse bietet Delegaten für benutzerdefinierte Anfrage- und Antwortverarbeitung.
    
    Die Änderungen legen die Grundlage für erweiterbare und anpassbare HTTP-Kommunikation in WCF-Umgebungen.

---
+++ IRH.Lib/Class/Deployment/VMWare/VMWareInterceptingHttpMessageHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Lib.Class.Deployment.VMWare
+{
+    internal class VMWareInterceptingHttpMessageHandler : DelegatingHandler
+    {
+        internal VMWareInterceptingHttpMessageHandler(HttpMessageHandler innerHandler, VMwareHttpMessageHandlerBehavior parent)
+        {
+            InnerHandler = innerHandler;
+            _parentHandler = parent;
+        }
+
+        private readonly VMwareHttpMessageHandlerBehavior _parentHandler;
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (_parentHandler.OnSending is not null)
+            {
+                HttpResponseMessage shortCircuitResponse = _parentHandler.OnSending(request, cancellationToken);
+                if (shortCircuitResponse is not null)
+                {
+                    return shortCircuitResponse;
+                }
+            }
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+            if (_parentHandler.OnSent is not null)
+            {
+                return _parentHandler.OnSent(response, cancellationToken);
+            }
+
+            return response;
+        }
+    }
+}
