34f25021e88e9e252705fc8eef2b405f637a104d
Niels <niels.schuler@web.de>
2025-11-22 20:41:09 +0100

    Neue Klasse `EsxiNavigation` hinzugefügt
    Die Klasse `EsxiNavigation` wurde im Namespace `IRH.Lib.Model.Deployment.Esxi` erstellt. Sie enthält drei öffentliche Eigenschaften: `Client` (vom Typ `VimPortTypeClient`), `UserSession` (vom Typ `UserSession`) und `ServiceContent` (vom Typ `ServiceContent`).
    
    Zusätzlich wurden mehrere `using`-Anweisungen hinzugefügt, um benötigte Namespaces wie `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, `System.Threading.Tasks` und `IRH.Lib.VMWare.Eight` einzubinden.

---
+++ IRH.Lib/Model/Deployment/Esxi/EsxiNavigation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRH.Lib.VMWare.Eight;
+
+namespace IRH.Lib.Model.Deployment.Esxi
+{
+    public class EsxiNavigation
+    {
+        public VimPortTypeClient Client { get; set; }
+        public UserSession UserSession { get; set; }
+        public ServiceContent ServiceContent { get; set; }
+    }
+}
