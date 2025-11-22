0d5e91a173420d7768efb08e91ecaf72d4e1e428
Niels <niels.schuler@web.de>
2025-11-22 15:12:38 +0100

    Füge HypervisorLoginInfo-Klasse und WebScheme-Enum hinzu
    In `HypervisorLoginInfo.cs` wurde die Klasse `HypervisorLoginInfo`
    hinzugefügt, die die Eigenschaften `Address`, `Port`, `Scheme`,
    `User` und `Password` enthält. Diese Klasse ist im Namespace
    `IRH.Lib.Model.Deployment.Esxi` definiert.
    
    In `WebScheme.cs` wurde der Enum-Typ `WebScheme` hinzugefügt,
    der die Werte `Http` und `Https` enthält. Dieser Enum ist im
    Namespace `IRH.Lib.Model.General` definiert.
    
    Zusätzlich wurden die notwendigen `using`-Anweisungen für die
    entsprechenden Namespaces ergänzt.

---
+++ IRH.Lib/Model/Deployment/Esxi/HypervisorLoginInfo.cs
@@ -0,0 +1,18 @@
+﻿using IRH.Lib.Model.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Lib.Model.Deployment.Esxi
+{
+    public class HypervisorLoginInfo
+    {
+        public required string Address { get; set; }
+        public required int Port { get; set; }
+        public required WebScheme Scheme { get; set; }
+        public required string User { get; set; }
+        public required string Password { get; set; }
+    }
+}
