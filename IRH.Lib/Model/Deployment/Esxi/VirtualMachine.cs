406a03d2a83cb48ff4fd60e54a06b9440ed1292a
Niels Schuler <niels.schuler@web.de>
2025-11-23 20:40:31 +0100

    Füge neue Klassen und Enum für ESXi-Deployment hinzu
    Ein neues `namespace` namens `IRH.Lib.Model.Deployment.Esxi` wurde hinzugefügt.
    Darin wurde ein `enum` namens `GuestOs` erstellt, der aktuell den Wert `Windows` enthält.
    
    Die Klasse `GuestOsLoginInfo` wurde hinzugefügt, um Anmeldeinformationen
    für Gastbetriebssysteme zu speichern (`User`, `Password`, `Domain`).
    
    Die Klasse `VirtualMachine` wurde hinzugefügt, die Eigenschaften wie
    `Id`, `Name` und `VM` (vom Typ `ObjectContent`) enthält, um virtuelle
    Maschinen zu repräsentieren.

---
+++ IRH.Lib/Model/Deployment/Esxi/VirtualMachine.cs
@@ -0,0 +1,16 @@
+﻿using IRH.Lib.VMWare.Eight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRH.Lib.Model.Deployment.Esxi
+{
+    public class VirtualMachine
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required ObjectContent VM { get; set; }
+    }
+}
