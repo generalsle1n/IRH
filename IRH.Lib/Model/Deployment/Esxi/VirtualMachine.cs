7d28e261c78d0e3fa5f39968f8c0363059a69dfa
Niels Schuler <niels.schuler@web.de>
2025-11-24 21:41:26 +0100

    Added LoginInfo as Property

--- IRH.Lib/Model/Deployment/Esxi/VirtualMachine.cs
+++ IRH.Lib/Model/Deployment/Esxi/VirtualMachine.cs
@@ -13,5 +13,6 @@ namespace IRH.Lib.Model.Deployment.Esxi
         public required string Name { get; set; }
         public required ObjectContent VM { get; set; }
         public required VirtualMachineGuestFileTransfer GuestFileTransfer { get; set; }
+        public GuestOsLoginInfo LoginInfo { get; set; }
     }
 }
